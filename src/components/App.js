305f680d16a9c181689f6a7dfb65b5bbc2418e06
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2019-01-22 14:53:25 -0800

    display navbar on app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from './NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar />
         <header className="App-header">
           <h1>Ashi</h1>
         </header>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,45 @@ class NavBar extends Component {
   render() {
     return (
       <div className="NavBar">
+      <nav
+      id="top-navbar"
+      className="navbar navbar-expand-lg"
+    >
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a id="brand1" className="nav-link" href="">
+              <i className="fa fa-instagram" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a id="brand2" className="nav-link" href="#">
+              <img style={{ width: 70 }} alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
 
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-compass" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-heart-o" aria-hidden="true" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a id="cart" className="nav-link">
+              <i className="fa fa-user-o" aria-hidden="true" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
       </div>
     );
   }
