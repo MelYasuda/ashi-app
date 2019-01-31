d6c80ba9afac51f7599c76184674d3e79e477a8f
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2019-01-31 08:28:37 -0800

    list listing in a city with no duplicates, fix variable names and install redux

--- src/components/Listings/Listing.js
+++ src/components/Listings/Listing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Listings.css'
 
 class Listing extends React.Component {
   constructor(props){
@@ -9,19 +10,32 @@ class Listing extends React.Component {
 
   render(){
     var value = this.props.value;
-    console.log(this.props.value)
+    var imgUrl = value["imageUrl"];
     return(
-
-      <ul>
-      { 
-        Object.keys(value).map((data, index) => (
-        <li>{value[data]}</li>
-        )
-      )     
-      }
-      </ul>
+      <div className='card-deck' style={{ marginTop: 15 }}>
+        <div className="card" style={{ width: 18 + "em", marginRight: 40 }}>
+          <img className="card-img-top" id="imgUrl" src={imgUrl} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{value["Title"]}</h5>
+            <p className="card-text">
+            ${value["Rent"]}
+            </p>
+            <button className="btn btn-primary">
+              Details
+            </button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
+// { 
+//   Object.keys(value).map((data, index) => (
+//   <li>{value[data]}</li>
+//   )
+// )     
+// }
+
 
 export default Listing;
--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -8,39 +8,92 @@ class Listings extends React.Component {
     super(props);
     this.state = {
       results: null,
-      isLoading: true
+      isLoading: true,
+      searchQuery: {
+        country: null,
+        city: null
+      }
     }
-    // var database = firebase.database();
 
-    new Promise((resolve, reject)=>{firebase.database().ref("Posts/United States/Boston/").on('value',  (snapshot) => {
+    const database = firebase.database();
+
+    const posts =  () => {
+      return new Promise((resolve, reject)=>{database.ref("Posts/United States/Boston/").on('value',  (snapshot) => {
       const value = snapshot.val();
-      var data = []
-      for(var key in value) {
-        var subValue = value[key];
-        for(var subKey in subValue){
-          var subSubValue = subValue[subKey];
-          for(var subSubKey in subSubValue) {
-            data.push(subSubValue[subSubKey]);
+      const listings = [];
+      const listingKeys = [];
+      for(const uid in value) {
+        const subValue = value[uid];
+        for(const categoryKey in subValue){
+          const subSubValue = subValue[categoryKey];
+          for(const listingKey in subSubValue) {
+            const listing = subSubValue[listingKey];
+            listingKeys.push(listingKey);
+            listings.push(listing);
           }
         }
       }
-      this.setState({results: data})
-      resolve()
+      const containerObject = {}
+      containerObject["listings"] = listings;
+      containerObject["listingKeys"] = listingKeys;
+      resolve(containerObject)
     })
-  }).then(()=> {
-    this.setState( {isLoading: false} )
+  })}
+
+    const newPosts = (containerObject) => {
+      return new Promise((resolve, reject)=>{database.ref("NewPosts/").on('value',  (snapshot) => {
+      const value = snapshot.val();
+      for(const uid in value) {
+        const subValue = value[uid];
+        for(const categoryKey in subValue){
+          const subSubValue = subValue[categoryKey];
+          for(const listingKey in subSubValue) {
+            const listing = subSubValue[listingKey]
+              if((listing["Location Preffered"]==="Boston" || listing["CityName"]==="Boston") && !containerObject["listingKeys"].includes(listingKey) ){
+                containerObject["listings"].push(listing);
+            }
+          }
+        }
+      }
+      resolve(containerObject)
+    })
+  })}
+
+const reservePost = (containerObject) => {
+  return new Promise((resolve, reject)=>{database.ref("ReservePost/").on('value',  (snapshot) => {
+    const value = snapshot.val();
+    for(const uid in value) {
+      const subValue = value[uid];
+      for(const subKey in subValue){
+          const listing = subValue[subKey]
+          if((listing["Location Preffered"]==="Boston" || listing["CityName"]==="Boston") && !containerObject["listingKeys"].includes(subKey)){
+            containerObject["listings"].push(listing);
+          }
+      }
+    }
+    this.setState({results: containerObject["listings"]})
+    resolve()
   })
+})
+}
+
+  const setLoadingState = () => {
+    this.setState( {isLoading: false} )
   }
 
+  posts().then(newPosts).then(reservePost).then(setLoadingState);
+
+  //end of constructor
+  }
 
   render(){
     if(this.state.isLoading) return null;
     return(
       <div className='container'>
-        <div className='row'>
-          <h1>List</h1>
+        <div className='row' style={{paddingLeft: '8%'}}>
+          <h1>Boston</h1>
           {this.state.results.map((result, index)=> 
-              
+
               <Listing value={result}/>
             )}
         </div>
