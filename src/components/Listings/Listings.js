741a59aabfaefed49e9a213587b48e5658cf6412
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2019-02-01 11:16:22 -0800

    enable search with lowercase

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -15,14 +15,14 @@ class Listings extends React.Component {
 
     const database = firebase.database();
 
-    const searchQuery = this.props.listings;
-    console.log(searchQuery);
+    const searchQuery = this.props.listings.city;
+    const upper = searchQuery.replace(/^\w/, c => c.toUpperCase());
 
     const posts =  () => {
-      return new Promise((resolve, reject)=>{database.ref("Posts/United States/" + searchQuery.city + "/").on('value',  (snapshot) => {
+      return new Promise((resolve, reject)=>{database.ref("Posts/United States/" + upper + "/").on('value',  (snapshot) => {
       let value = snapshot.val();
       if (!value){
-        database.ref("Posts/Canada/" + searchQuery.city + "/").on('value',  (snapshot) =>{
+        database.ref("Posts/Canada/" + upper + "/").on('value',  (snapshot) =>{
           value = snapshot.val()
         })
       }
@@ -56,7 +56,7 @@ class Listings extends React.Component {
           const subSubValue = subValue[categoryKey];
           for(const listingKey in subSubValue) {
             const listing = subSubValue[listingKey]
-              if((listing["Location Preffered"]===searchQuery.city || listing["CityName"]===searchQuery.city) && !containerObject["listingKeys"].includes(listingKey) ){
+              if((listing["Location Preffered"]===upper || listing["CityName"]===upper) && !containerObject["listingKeys"].includes(listingKey) ){
                 containerObject["listings"].push(listing);
             }
           }
@@ -73,7 +73,7 @@ const reservePost = (containerObject) => {
       const subValue = value[uid];
       for(const subKey in subValue){
           const listing = subValue[subKey]
-          if((listing["Location Preffered"]===searchQuery.city || listing["CityName"]===searchQuery.city) && !containerObject["listingKeys"].includes(subKey)){
+          if((listing["Location Preffered"]===upper || listing["CityName"]===upper) && !containerObject["listingKeys"].includes(subKey)){
             containerObject["listings"].push(listing);
           }
       }
@@ -98,7 +98,7 @@ const reservePost = (containerObject) => {
     return(
       <div className='container'>
         <div className='row' style={{paddingLeft: '8%'}}>
-          <h1>Boston</h1>
+          <h1>{this.props.listings.city}</h1>
           {this.state.results.map((result, index)=> 
 
               <Listing value={result}/>
